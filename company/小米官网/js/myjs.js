17e46d1e3c342b012ec145674e17782bad904079
yyyzsn <1311543277@qq.com>
2016-09-15 21:05:41 +0800

    xiaomi

---
+++ company/小米官网/js/myjs.js
@@ -0,0 +1,17 @@
+function list1(){
+	var nav=document.getElementById('nav-menu-list1');
+	nav.style.display="block";
+	var link=document.getElementById('nav-link');
+	link.style.width="1224px";
+	link.style.height="300px";
+}
+
+function list11(){
+	var nav=document.getElementById('nav-menu-list1');
+	nav.style.display="none";
+}
+function list12(){
+//	var nav=document.getElementById('nav-menu-list1');
+//	nav.style.display="block";
+	
+}
